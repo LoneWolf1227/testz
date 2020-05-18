75ae0ebc9bc08824f635e3c86ad26ab1914278e4
Alone Wolf <manalonewolf@gmail.com>
2020-05-18 18:15:27 +0500

    testz

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 
 return [
     'host' => "localhost",
-    'dbname' => "id13554751_project_test",
-    'user' => "id13554751_wolfalone",
-    'pass' => "O-6fBEe+%p*GP#+j",
+    'dbname' => "project_test",
+    'user' => "root",
+    'pass' => "",
 ];
